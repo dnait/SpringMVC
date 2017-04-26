8997ebc4abd9fc89737bebe49ae8b2904b97fa91
Katie <raokatie324@gmail.com>
2017-04-26 15:28:34 -0400

    Add JSP sample

--- data_App/src/main/java/webapp/LoginServlet.java
+++ data_App/src/main/java/webapp/LoginServlet.java
@@ -3,6 +3,7 @@ package webapp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,22 +28,26 @@ import javax.servlet.http.HttpServletResponse;
 //1. extends javax.servlet.http.HttpServlet
 //2. @WebServlet(urlPatterns = "/login.do")
 //3. doGet(HttpServletRequest request, HttpServletResponse response)
-//4. How is the response created?
+
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Yep!!!!!!!!</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("Servlet is working");
-		out.println("</body>");
-		out.println("</html>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//		PrintWriter out = response.getWriter();
+//		out.println("<html>");
+//		out.println("<head>");
+//		out.println("<title>Yep!!!!!!!!</title>");
+//		out.println("</head>");
+//		out.println("<body>");
+//		out.println("Servlet is working");
+//		out.println("</body>");
+//		out.println("</html>");
+		
+		String name = request.getParameter("name");
+		request.setAttribute("name", name);
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 
 	}
 
